b9e51a4977ec85c8435da67dd999194cc4b273f3
Himanshu <38362189+himanshu1755@users.noreply.github.com>
2023-01-20 21:54:35 +0530

    added log4j2 functionality

--- src/main/java/com/example/mqtt/MqttApplication.java
+++ src/main/java/com/example/mqtt/MqttApplication.java
@@ -1,13 +1,16 @@
 package com.example.mqtt;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class MqttApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MqttApplication.class, args);
+		log.info("mw");
 	}
 
 }
